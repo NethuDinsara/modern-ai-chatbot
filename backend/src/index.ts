92f7e9d315b2708ca5060f312662eccbcedaf563
NethuDinsara <neth.hettihewa@gmail.com>
2025-10-12 12:03:39 +0530

    index ts updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,16 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import express from "express"
+
+const app=express();
+
+//GET - 
+//PUT
+//POST
+//DELETE
+
+//routes endpoints
+app.get("/hello",(req,res,next)=>{
+  return res.send("Hello");
+})
+
+
+app.listen(5001,()=>console.log("Server Open"))
