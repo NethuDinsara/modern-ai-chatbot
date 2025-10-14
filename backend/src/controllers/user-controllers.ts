f7a35fff6471c9f36cc22a8ab3feb4478df7d7ba
NethuDinsara <neth.hettihewa@gmail.com>
2025-10-14 23:19:47 +0530

    updaed user controller

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,20 @@
-export const getAllUsers =() =>{
-    //get all users
+//in here wants to find all the users from the database
+
+import { Request, Response, NextFunction } from "express";
+import User from "../models/User.js"
+
+export const getAllUsers = async(
+    req:Request,
+    res:Response,
+    next:NextFunction
+) => {
+    //finding all users
+    try{
+        const users= await User.find(); //find from mongoose.model it will find all users
+        return res.status(200).json({message:"OK",users});
+
+    }catch(error){
+        console.log(error);
+        return res.status(200).json({message:"ERROR", cause:error.message})
+    }
 }
