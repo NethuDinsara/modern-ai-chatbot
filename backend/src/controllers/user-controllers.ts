38cd606374b61dc73d749748fbbe91b9beca703a
NethuDinsara <neth.hettihewa@gmail.com>
2025-10-15 20:42:36 +0530

    updated user-controllers

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,7 +1,9 @@
 //in here wants to find all the users from the database
 
 import { Request, Response, NextFunction } from "express";
-import User from "../models/User.js"
+import User from "../models/User.js";
+import {hash} from 'bcrypt';
+
 
 export const getAllUsers = async(
     req:Request,
@@ -17,4 +19,24 @@ export const getAllUsers = async(
         console.log(error);
         return res.status(200).json({message:"ERROR", cause:error.message})
     }
-}
+};
+
+export const userSigUp = async(
+    req:Request,
+    res:Response,
+    next:NextFunction
+) => {
+    //user sign up
+    try{
+
+        const {name,email,password} =req.body;
+        const hashedPassword = await hash(password,10);
+        const user= new User({name,email,password:hashedPassword}); //new instance of user will create 
+        await user.save(); 
+        return res.status(201).json({message:"OK", id: user._id.toString() });
+
+    }catch(error){
+        console.log(error);
+        return res.status(200).json({message:"ERROR", cause:error.message})
+    }
+};
