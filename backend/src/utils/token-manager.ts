179382ea6517af174348d5a3d98eb2ace1d3f15e
NethuDinsara <neth.hettihewa@gmail.com>
2025-10-17 23:15:46 +0530

    creating and updating token-manager.ts

---
+++ backend/src/utils/token-manager.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken'
+
+//making a new token
+export const   createToken = (id: String,email:string,expiresIn) =>{
+    const payload= {id,email};//an object
+    const token= jwt.sign(payload,process.env.JWT_SECRET,{
+        expiresIn:"7d",
+    });//creating a new token
+
+    return token;
+
+}
