d3f407dc732ae370d5e61c1b0aeb07a00e1114d6
NethuDinsara <neth.hettihewa@gmail.com>
2025-11-11 00:40:19 +0530

    Updated the main routing for App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import Header from "./components/Header"
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/SignUp"
+import Chat from "./pages/Chat"
+import NotFound from "./pages/NotFound"
 
 function App() {
 
-  return <main> 
-
-  </main>;
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Login />}/>
+        <Route path="/" element={<Signup />}/>
+        <Route path="/" element={<Chat />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </main>
+  )
 }
 
 export default App
