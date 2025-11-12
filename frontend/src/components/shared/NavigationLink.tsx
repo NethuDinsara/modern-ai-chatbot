be8196b5197caf93d07dda97624f028fc493d209
NethuDinsara <neth.hettihewa@gmail.com>
2025-11-13 00:34:40 +0530

    created aNavigationLink.tsx

---
+++ frontend/src/components/shared/NavigationLink.tsx
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom'
+
+type Props ={
+    to: string;
+    bg: string;
+    text: string;
+        textColor: string;
+    onClick?:()=>Promise<void>;//since we dont wanna return we use void
+};
+
+const NavigationLink = (props:Props) => {
+  return(
+    <Link 
+      to={props.to} 
+      style={{background:props.bg, color:props.textColor}}
+    >
+      { props.text}
+    </Link>
+  ) 
+};
+
+export default NavigationLink;
