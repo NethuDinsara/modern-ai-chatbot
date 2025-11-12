2372e361d7af2c22398364a16552fc8622f0ee97
NethuDinsara <neth.hettihewa@gmail.com>
2025-11-13 00:43:27 +0530

    small bug spelling issue debugged

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header"
 import {Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import Signup from "./pages/SignUp"
+import Signup from "./pages/Signup"
 import Chat from "./pages/Chat"
 import NotFound from "./pages/NotFound"
 
