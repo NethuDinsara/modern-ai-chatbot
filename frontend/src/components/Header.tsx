8fe9c1b9df4fb3d00c3f3e5e1392a9c92d8efd72
NethuDinsara <neth.hettihewa@gmail.com>
2025-11-16 19:26:35 +0530

    updated header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,12 @@
 import { AppBar, Toolbar } from '@mui/material'
 import React from 'react'
 import Logo from './shared/Logo'
+import { useAuth } from '../context/AuthContext';
 
 
 const Header = () => {
+
+  const auth = useAuth();
   return (
     <AppBar 
         sx={{bgcolor:"transparent", position:"static", boxShadow:"none"}}
@@ -11,10 +14,13 @@ const Header = () => {
 
         <Toolbar sx={{display:"flex"}}>
             <Logo />
+            <div>/////////
+
+            </div>
         </Toolbar>
         
     </AppBar>
   )
-}
+};
 
 export default Header
